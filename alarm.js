54d0f5e9e9c28d40a9e2e1ab25d368f4ed63825f
Elizabeth Lotto <elizlotto@gmail.com>
2020-07-23 13:37:31 -0400

    working

--- alarm.js
+++ alarm.js
@@ -4,15 +4,15 @@ chrome.alarms.onAlarm.addListener(function () {
   chrome.notifications.create({   //creating our notification and adding info
     type: 'basic',
     iconUrl: 'logo.png',
-    title: 'Time to move around!',
-    message: 'url to workout here?',
-    buttons: [{ title: 'Alright!!' }],
+    title: 'Alarm',
+    message: 'It is time to get up and move!',
+    buttons: [{ title: 'Ok!' }],
     priority: 0, //default is 0
   });
 });
 
-chrome.notifications.onButtonClicked.addListener(function () {
-  chrome.storage.sync.get(['minutes'], function (item) {
-    chrome.alarms.create({ delayInMinutes: item.minutes });
-  });
-});
+// chrome.notifications.onButtonClicked.addListener(function () {
+//   chrome.storage.sync.get(['minutes'], function (item) {
+//     chrome.alarms.create({ delayInMinutes: item.minutes });
+//   });
+// });
--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 'use strict';
 
+//let setting = null;
+let minutes;
 function setAlarm(event) { //this function will set times 
-  //console.log("inside alarm")
-  //console.log(event, "event")
-  let minutes = parseFloat(event.target.value);
+  minutes = parseFloat(event.target.value);
+  alarmSet.innerHTML = `You will receive a reminder notification every ${minutes} minutes. :)`;
   chrome.alarms.create({ delayInMinutes: minutes, periodInMinutes: minutes });
   chrome.storage.sync.set({ minutes: minutes }); //can take in two args, the second could be a callback
-  window.close(); //can only be used on windows opened by a window.open() method.
+  //window.close(); //can only be used on windows opened by a window.open() method.
 }
-
 
 function clearAlarm() {
   chrome.alarms.clearAll();
   window.close();
 }
 
+let alarmSet = document.querySelector('.alarmSet');
+
+// function previousAlarm(input) {
+//   if (minutes) alarmSet.innerHTML = `Your notifications are set for ${minutes}`;
+// }
+
+// previousAlarm(minute);
+
 document.getElementById('15min').addEventListener('click', setAlarm); //Cannot read property 'addEventListener' of null 
-document.getElementById('30min').addEventListener('click', setAlarm); 
+document.getElementById('30min').addEventListener('click', setAlarm); //when clicked twice, it seems to add up the times? ie 15 pressed twice gives 30 min alarms
+document.getElementById('60min').addEventListener('click', setAlarm);
 document.getElementById('cancelAlarm').addEventListener('click', clearAlarm);
 
-//think the invocation of alarm is either not happening or nothing is being passed in somehow to eventlistener.
+
+
+//app does give notification reminders to move around but not at the times set. Are the times accumulating when you press on it?
+//how can we check the input? 
+//is there a way we can add an input that allows us to see what was set?
